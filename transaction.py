94c5944d95ce6666acd8f099a98caf99765de357
Bertrand Nzomigni <bertrand.nzomigni@gmail.com>
2025-07-12 14:35:22 +0200

    Début classe transaction

--- transaction.py
+++ transaction.py
@@ -3,14 +3,28 @@ class Transaction:
         self.joueur = joueur
         self.montant = montant
         self.lieu = lieu
-        self.possible = False
     def verifier_appliquer(self):
-        self.possible = self.verifier()
-        if self.possible:
+        if self.verifier():
             self.appliquer()
+            return True
+        else:
+            return False
 
 class AchatMarchandises(Transaction):
-    def __init__(self, joueur,montant,lieu,quantite,prix,bateau):
+    def __init__(self, joueur,montant,lieu,quantite,nom_marchandise,prix,bateau):
         super().__init__(joueur, montant,lieu,quantite,prix)
         self.quantite = quantite
         self.prix = prix
+        self.bateau = bateau
+        self.nom_marchandise = nom_marchandise
+        self.erreur = ""
+    def verifier(self):
+        self.cout_total = self.quantite * self.prix
+        volume_total = self.quantite * MARCHANDISES[self.nom_marchandise]["volume"]
+        if self.joueur.obtenir_florins() < self.cout_total:
+            self.erreur = "Pas assez d'argent"
+            return False
+        elif self.bateau.capacite - self.bateau.obtenir_volume_utilise() < volume_total:
+            self.erreur = "Pas assez de place dans la soute."
+            return False
+        return True
