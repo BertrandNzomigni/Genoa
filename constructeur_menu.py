d11630733096411a79b74471fec8581314733e90
IndianaBob2 <154674015+IndianaBob2@users.noreply.github.com>
2025-07-12 12:34:33 +0200

    Correction majuscule

--- constructeur_menu.py
+++ constructeur_menu.py
@@ -37,7 +37,7 @@ class ConstructeurMenu:
         return MenuVente(self.monde, self.jeu, self.bateau)
 
     def construire_menu_achat_bateaux(self):
-        return menuAchatBateaux(self.monde,self.jeu,self.joueur,self.joueur.obtenir_lieu())
+        return MenuAchatBateaux(self.monde,self.jeu,self.joueur,self.joueur.obtenir_lieu())
 
 
 
