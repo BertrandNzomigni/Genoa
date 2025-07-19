be52916b6c7b592b00e39d25b950e1dfbdec8498
Bertrand Nzomigni <bertrand.nzomigni@gmail.com>
2025-07-19 13:31:10 +0200

    Changement IHM, tests et to_do

--- IHM.py
+++ IHM.py
@@ -1,9 +1,11 @@
 from tkinter import Tk
 
 class Fenetre(Tk):
-    def __init__(self):
+    def __init__(self,titre = None):
         super().__init__()
+        if titre is not None:
+            self.changer_titre(titre)
     def changer_titre(self,titre):
-        pass
+        self.title(titre)
     def demarrer(self):
         self.mainloop()
--- test_ihm.py
+++ test_ihm.py
@@ -7,6 +7,16 @@ class TestIHM(unittest.TestCase):
         f = Fenetre()
         f.demarrer()
         self.assertEqual(input("Est qu'une fenêtre s'est affiché ? (y/n) : ").upper(),"Y","La fenêtre n'est pas affiché.")
+    def test_titre(self):
+        f = Fenetre()
+        f.changer_titre("Marchand de Gênes")
+        f.demarrer()
+        self.assertEqual(input("Est qu'une fenêtre avec pour titre 'Marchand de Gênes' s'est affiché ? (y/n) : ").upper(),"Y","La fenêtre avec titre n'est pas affiché.")
+    def test_titre2(self):
+        f = Fenetre("Marchand de Gênes")
+        f.demarrer()
+        self.assertEqual(input("Est qu'une fenêtre avec pour titre 'Marchand de Gênes' s'est affiché ? (y/n) : ").upper(),"Y","La fenêtre avec titre n'est pas affiché.")
+        
 
 if __name__ == '__main__':
     unittest.main()
