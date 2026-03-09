a4c73303549e6568e133f8a66f5c37c110091be2
Bertrand Nzomigni <bertrand.nzomigni@gmail.com>
2026-03-09 20:05:07 +0100

    - Création de la classe AfficheurBateauGlobaux - Simplification de la classe MenuBateauGlobaux

--- Menus/MenuBateauxGlobal.py
+++ Menus/MenuBateauxGlobal.py
@@ -1,14 +1,13 @@
 from Menus.Menu import Menu
-from Menus.Option import Option
+from Menus.Patron.Affichage.AfficheurBateauxGlobaux import AfficheurBateauGlobaux
 from Menus.Patron.Options.PatronBateauxGlobaux import PatronOptionsBateauxGlobaux
 
 class MenuBateauxGlobal(Menu):
     def __init__(self,joueur,monde,jeu):
         Menu.__init__(self,monde,jeu)
         self.joueur = joueur
+        self.afficheur = AfficheurBateauGlobaux(self.monde,self.joueur)
     def charger_options(self):
         self.ajouter_plusieurs_options(PatronOptionsBateauxGlobaux(self.jeu).options)
     def afficher_corps(self):
-        print("Liste des bateaux")
-        for bateau in self.joueur.obtenir_bateaux():
-            print(f"{bateau.obtenir_nom()} : {bateau.obtenir_lieu().obtenir_nom()}")
+        self.afficheur.afficher()
---
+++ Menus/Patron/Affichage/AfficheurBateauxGlobaux.py
@@ -0,0 +1,11 @@
+from .Afficheur import Afficheur
+
+class AfficheurBateauGlobaux(Afficheur):
+    def __init__(self,monde,joueur):
+        super().__init__()
+        self.monde = monde
+        self.joueur = joueur
+    def afficher(self):
+        print("Liste des bateaux")
+        for bateau in self.joueur.obtenir_bateaux():
+            print(f"{bateau.obtenir_nom()} : {bateau.obtenir_lieu().obtenir_nom()}")
