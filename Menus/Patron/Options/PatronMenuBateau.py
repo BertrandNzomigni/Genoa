5f1482e18819104013e27b76efd8de8ee4f3151c
Bertrand Nzomigni <bertrand.nzomigni@gmail.com>
2026-04-28 14:27:50 +0200

    Le joueur peut quitter le bateau

--- Menus/Patron/Options/PatronMenuBateau.py
+++ Menus/Patron/Options/PatronMenuBateau.py
@@ -11,8 +11,11 @@ class PatronMenuBateau(Patron):
 
     def creer_options(self):
         super().creer_options()
-
-        self.options.append(Option("Diriger le bateau",[self.joueur.rejoint_bateau,self.bateau]))
+        
+        if self.joueur.bateau_dirige == None:
+            self.options.append(Option("Diriger le bateau",[self.joueur.rejoint_bateau,self.bateau]))
+        elif self.joueur.bateau_dirige == self.bateau:
+            self.options.append(Option("Quitter le bateau",[self.joueur.quitte_bateau]))
         self.options.append(Option("Quitter",[self.jeu.changer_menu_actif,self.constructeur_menu.construire_menu_bateaux()]))
         return self.options
     
